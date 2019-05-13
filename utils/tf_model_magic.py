de63535212df6319e84668477eaaa5d9d7cd7ff6
王德勋 <404904489@qq.com>
2019-05-13 13:53:19 +0800

    Implement removing one layer like dropout from tf's pb

--- utils/tf_model_magic.py
+++ utils/tf_model_magic.py
@@ -26,13 +26,11 @@ def remove_layer_from_pb():
     args = parser.parse_args()
     pb_path = args.pb
 
-    sess = tf.InteractiveSession()
     with tf.gfile.GFile(pb_path, 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
-        tf.import_graph_def(graph_def, name='')
 
-    for idx, node in enumerate(sess.graph_def.node):
+    for idx, node in enumerate(graph_def.node):
         print(idx, node.name, node.op, sep='  ')
     pdb.set_trace()
 
@@ -40,18 +38,18 @@ def remove_layer_from_pb():
     start_idx, end_idx = list(map(int, inputs.split(' ')))
 
     new_node = node_def_pb2.NodeDef()
-    new_node.CopyFrom(sess.graph_def.node[end_idx])
+    new_node.CopyFrom(graph_def.node[end_idx])
     print('Old input of the end node:\n', new_node.input)
     new_input = new_node.input[:].copy()
 
     drop_layer_name = next(filter(lambda i: i.split('/')[0] != new_node.name.split('/')[0], new_input))
-    new_input[new_input.index(drop_layer_name)] = sess.graph_def.node[start_idx].name
+    new_input[new_input.index(drop_layer_name)] = graph_def.node[start_idx].name
     drop_layer_name = drop_layer_name.split('/')[0]
     del new_node.input[:]
     new_node.input.extend(new_input)
     print('New input of the end node:\n', new_node.input)
 
-    new_nodes = sess.graph_def.node[:].copy()
+    new_nodes = graph_def.node[:].copy()
     new_nodes[end_idx] = new_node
     new_nodes = [n for n in new_nodes if n.name.split('/')[0] != drop_layer_name]
 
@@ -63,8 +61,6 @@ def remove_layer_from_pb():
 
     with tf.gfile.GFile(os.path.splitext(pb_path)[0] + '_removed.pb', 'wb') as f:
         f.write(output_graph_def.SerializeToString())
-
-    sess.close()
 
 
 if __name__ == '__main__':
