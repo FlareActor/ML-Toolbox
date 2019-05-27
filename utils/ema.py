f2c8e8d0910005958143802e0111b00c3661085c
FlareActor <404904489@qq.com>
2019-05-28 01:05:32 +0800

    Update for ema

--- utils/ema.py
+++ utils/ema.py
@@ -1,38 +1,66 @@
 import torch
-from torch import nn
+import torch.nn as nn
 
-class EMA(object):
-    def __init__(self, decay):
+
+class ExponentialMovingAverage(object):
+    def __init__(self, decay=0.9995, dynamic=True):
         self.decay = decay
+        self.dynamic = dynamic
         self.shadow_params = {}
+        self.step = 0
     
     def register(self, model, clear=False):
         if clear:
             self.shadow_params.clear()
-        for n,p in model.named_parameters():
+            self.step = 0
+        for n, p in model.named_parameters():
             if p.requires_grad:
                 self.shadow_params[n] = p.data.clone()
 
     def update(self, model):
-        for n,p in model.named_parameters():
+        self.step += 1
+        if self.dynamic:
+            decay = min((1 + self.step) / (10 + self.step), self.decay)
+        else:
+            decay = self.decay
+            
+        for n, p in model.named_parameters():
             if p.requires_grad:
-                assert n in self.shadow_params
-                new_param = self.decay*self.shadow_params[n]
-                new_param += (1-self.decay)*p.data
-                self.shadow_params[n] = new_param.clone()
+                new_param = decay * self.shadow_params[n] + (1 - decay) * p.data
+                self.shadow_params[n] = new_param
+                
+    def exchange(self, model):
+        for n, p in model.named_parameters():
+            if p.requires_grad:
+                tmp = self.shadow_params[n]
+                self.shadow_params[n] = p.data
+                p.data = tmp
+        return model
 
+                
 if __name__=='__main__':
     class TestModel(nn.Module):
         def __init__(self, init_value):
             super(TestModel, self).__init__()
             self.linear1 = nn.Linear(6, 2, bias=False)
             nn.init.constant_(self.linear1.weight.data, init_value)
+            
     model1 = TestModel(3)
     model2 = TestModel(init_value=5)
     for n,p in model1.named_parameters():
         print(n, p)
-    ema = EMA(0.95)
+        
+    ema = ExponentialMovingAverage(0.95)
     ema.register(model1, clear=True)
     print(ema.shadow_params)
     ema.update(model2)
     print(ema.shadow_params)
+    
+    print('\n')
+    ema_model = ema.exchange(model1)
+    for n,p in ema_model.named_parameters():
+        print(n, p)
+    
+    model = ema.exchange(ema_model)
+    for n,p in model.named_parameters():
+        print(n, p)
